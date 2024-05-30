9cbdcf500191577f0589515f10d8f3df902a5211
Dennis Francis <dennisfrancis.in@gmail.com>
2024-05-30 15:07:55 +0530

    custom: add linux.el for linux specific vars
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

---
+++ custom/.emacs.d/linux.el
@@ -0,0 +1,6 @@
+;; Your Linux specific settings
+
+; (setenv "JAVA_HOME" "/usr/lib/jvm/java-14-openjdk-amd64")
+(setenv "JAVA_HOME"  "/usr/lib/jvm/java-1.21.0-openjdk-amd64")
+(setq lsp-java-java-path "/usr/lib/jvm/java-1.21.0-openjdk-amd64/bin/java")
+
